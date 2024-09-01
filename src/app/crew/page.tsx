7a51c9fc70e2a93c93287e48d93dec29991f510a
Ryan McGuire <rmva@ryans-mbp-2.myfiosgateway.com>
2024-09-01 16:50:45 -0400

    Crew Page Data Display

--- src/app/crew/page.tsx
+++ src/app/crew/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image';
 import bgDesktop from '/public/assets/crew/background-crew-desktop.jpg';
 import bgTablet from '/public/assets/crew/background-crew-tablet.jpg';
 import bgMobile from '/public/assets/crew/background-crew-mobile.jpg';
+import { data } from '@/util/data';
 
 export default function CrewPage() {
+	const content = data.crew;
 	return (
 		<main className='h-screen w-full p-24 absolute top-0 left-0'>
 			<Image
@@ -42,10 +44,29 @@ export default function CrewPage() {
 				}}
 				className='md:hidden'
 			/>
-			<div className='mx-w-screen-lg mx-auto mt-40 grid grid-cols-2 relative z-50'>
-				<div className='flex flex-col items-start justify-between '>
-					<h1>Technology</h1>
-				</div>
+			<div className='mx-w-screen-lg mx-auto mt-[75px] mb-16 flex justify-start items-center relative z-20'>
+				<h5 className='text-white'>
+					<span className='opacity-25 font-bold'>02</span> Meet Your Crew
+				</h5>
+			</div>
+			<div className='overflow-y-visible -mt-20'>
+				{content.map((c) => (
+					<div className='mx-w-screen-lg mx-auto grid grid-cols-2 relative z-20'>
+						<div className='flex flex-col justify-center items-start'>
+							<h4>{c.role}</h4>
+							<h3>{c.name}</h3>
+							<p className='text-lavender'>{c.bio}</p>
+						</div>
+						<div className='flex justify-center items-center'>
+							<Image
+								src={c.images.webp}
+								width={445}
+								height={445}
+								alt={`Satellite Image of ${c.name}`}
+							/>
+						</div>
+					</div>
+				))}
 			</div>
 		</main>
 	);
--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
+import { data } from '@/util/data';
 import bgDesktop from '/public/assets/destination/background-destination-desktop.jpg';
 import bgTablet from '/public/assets/destination/background-destination-tablet.jpg';
 import bgMobile from '/public/assets/destination/background-destination-mobile.jpg';
 
 export default function DestinationPage() {
+	const content = data.destinations;
 	return (
 		<main className='h-screen w-full p-24 absolute top-0 left-0'>
 			<Image
@@ -42,10 +44,41 @@ export default function DestinationPage() {
 				}}
 				className='md:hidden'
 			/>
-			<div className='mx-w-screen-lg mx-auto mt-40 grid grid-cols-2 relative z-50'>
-				<div className='flex flex-col items-start justify-between '>
-					<h1>Destination</h1>
-				</div>
+			<div className='mx-w-screen-lg mx-auto mt-[75px] mb-16 flex justify-start items-center relative z-20'>
+				<h5 className='text-white'>
+					<span className='opacity-25 font-bold'>01</span> Pick Your Destination
+				</h5>
+			</div>
+			<div className='overflow-hidden'>
+				{content.map((c) => (
+					<div className='mx-w-screen-lg mx-auto grid grid-cols-2 relative z-20'>
+						<div className='flex justify-center items-center'>
+							<Image
+								src={c.images.webp}
+								width={445}
+								height={445}
+								alt={`Satellite Image of ${c.name}`}
+							/>
+						</div>
+						<div className='flex flex-col justify-center items-start'>
+							<h2>{c.name}</h2>
+							<p className='text-lavender'>{c.description}</p>
+							<hr />
+							<div className='grid grid-cols-2 w-full'>
+								<div>
+									<p className='subheadingTwo text-lavender'>Avg. Distance</p>
+									<p className='subheadingOne'>{c.distance}</p>
+								</div>
+								<div>
+									<p className='subheadingTwo text-lavender'>
+										Est. Travel Time
+									</p>
+									<p className='subheadingOne'>{c.travel}</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				))}
 			</div>
 		</main>
 	);
