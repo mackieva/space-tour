3348522779877cdb8f906c55eba619ae27025afc
Ryan McGuire <rmva@ryans-mbp-2.myfiosgateway.com>
2024-09-01 14:14:33 -0400

    Logo Linking & Border Animation

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,13 +1,23 @@
+'use client';
+
 import logo from '/public/assets/shared/logo.svg';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import Nav from './Nav';
+import Link from 'next/link';
 
 export default function Header() {
 	return (
 		<header className='max-w-screen-lg mx-auto grid grid-cols-header mt-10 relative z-50'>
-			<div className='ml-12'>
-				<Image src={logo} alt="Space Tourism Logo - Let's Go" />
+			<div className='ml-12 flex justify-start gap-3 items-center'>
+				<Link href='/'>
+					<Image src={logo} alt="Space Tourism Logo - Let's Go" />
+				</Link>
+				<motion.div
+					className='logoBorder hidden lg:block'
+					initial={{ width: '0%' }}
+					animate={{ width: '100%' }}
+				/>
 			</div>
 			<Nav />
 		</header>
