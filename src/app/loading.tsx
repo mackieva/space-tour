6af2890a6707a6df5ce0cbcece22dd19e9d055c9
Ryan McGuire <rmva@ryans-mbp-2.myfiosgateway.com>
2024-09-01 17:54:40 -0400

    Tech Content

---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+export default function LoadingPage() {
+	return (
+		<div className='bg-blue h-screen w-full p-24 absolute top-0 left-0'></div>
+	);
+}
--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image';
 import bgDesktop from '/public/assets/technology/background-technology-desktop.jpg';
 import bgTablet from '/public/assets/technology/background-technology-tablet.jpg';
 import bgMobile from '/public/assets/technology/background-technology-mobile.jpg';
+import { data } from '@/util/data';
 
 export default function TechnologyPage() {
+	const content = data.technology;
 	return (
 		<main className='h-screen w-full p-24 absolute top-0 left-0'>
 			<Image
@@ -42,10 +44,32 @@ export default function TechnologyPage() {
 				}}
 				className='md:hidden'
 			/>
-			<div className='mx-w-screen-lg mx-auto mt-40 grid grid-cols-2 relative z-50'>
-				<div className='flex flex-col items-start justify-between '>
-					<h1>Technology</h1>
-				</div>
+			<div className='mx-w-screen-lg mx-auto mt-[75px] mb-16 flex justify-start items-center relative z-20'>
+				<h5 className='text-white'>
+					<span className='opacity-25 font-bold'>03</span> Space Launch 101
+				</h5>
+			</div>
+			<div className='overflow-y-visible -mt-20'>
+				{content.map((c) => (
+					<div
+						key={c.name}
+						className='mx-w-screen-lg mx-auto grid grid-cols-2 relative z-20'
+					>
+						<div className='flex flex-col justify-center items-start'>
+							<p className='secondaryTwo'>The Terminology...</p>
+							<h3>{c.name}</h3>
+							<p className='text-lavender'>{c.description}</p>
+						</div>
+						<div className='flex justify-center items-center'>
+							<Image
+								src={c.images.portrait}
+								width={445}
+								height={445}
+								alt={`Satellite Image of ${c.name}`}
+							/>
+						</div>
+					</div>
+				))}
 			</div>
 		</main>
 	);
