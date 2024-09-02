2526032e9710403478a97245c41899257a581f5d
Ryan McGuire <rmva@ryans-mbp-2.myfiosgateway.com>
2024-09-02 12:53:25 -0400

    nav click to open and close

--- src/components/ui/Nav.tsx
+++ src/components/ui/Nav.tsx
@@ -9,17 +9,26 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 
 export default function Nav() {
 	const [hoveredLink, setHoveredLink] = useState('');
+	const [navOpen, setNavOpen] = useState(false);
 
 	return (
 		<>
 			<div className='flex justify-end items-center md:hidden'>
-				<Bars3Icon className='size-10 text-lavender' />
+				<Bars3Icon
+					className='size-10 text-lavender hover:cursor-pointer'
+					onClick={() => setNavOpen(true)}
+				/>
 			</div>
 			<nav
-				className='flex flex-col justify-start items-start bg-white/[0.04] backdrop-blur-md gap-[32px] absolute top-0 right-0 w-2/3 h-screen p-8 pb-0 md:justify-center md:relative md:h-auto md:w-full md:flex-row md:translate-x-0 lg:gap-[52px]'
+				className={`flex flex-col justify-start items-start bg-white/[0.04] backdrop-blur-md gap-[32px] absolute top-0 right-0 w-2/3 h-screen p-8 pb-0 transition-all ${
+					navOpen ? 'translate-x-0' : 'translate-x-full'
+				} md:justify-center md:relative md:h-auto md:w-full md:flex-row md:translate-x-0 lg:gap-[52px]`}
 				onMouseLeave={() => setHoveredLink('')}
 			>
-				<XMarkIcon className='size-6 text-lavender self-end md:hidden' />
+				<XMarkIcon
+					className='size-6 text-lavender self-end hover:cursor-pointer md:hidden'
+					onClick={() => setNavOpen(false)}
+				/>
 				{navList.map((link, i) => (
 					<Link
 						className='font-sansCon text-white tracking-[2.7px] uppercase relative text-base md:text-sm md:pb-10 lg:text-base'
