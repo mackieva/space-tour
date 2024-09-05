2643c8bc1b7f72f6a651a60a55dc9b218fe21757
Ryan McGuire <rmva@ryans-mbp-2.myfiosgateway.com>
2024-09-05 11:24:18 -0400

    tablet work and prettier setup with tailwind

--- src/app/crew/page.tsx
+++ src/app/crew/page.tsx
@@ -1,117 +1,124 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import bgDesktop from '/public/assets/crew/background-crew-desktop.jpg';
-import bgTablet from '/public/assets/crew/background-crew-tablet.jpg';
-import bgMobile from '/public/assets/crew/background-crew-mobile.jpg';
-import { motion } from 'framer-motion';
-import { data } from '@/util/data';
-import { useState } from 'react';
+import Image from "next/image";
+import bgDesktop from "/public/assets/crew/background-crew-desktop.jpg";
+import bgTablet from "/public/assets/crew/background-crew-tablet.jpg";
+import bgMobile from "/public/assets/crew/background-crew-mobile.jpg";
+import { motion } from "framer-motion";
+import { data } from "@/util/data";
+import { useState } from "react";
 
 export default function CrewPage() {
-	const content = data.crew;
-	const [tab, setTab] = useState(0);
-	return (
-		<main className='h-screen w-full absolute top-0 left-0'>
-			<Image
-				src={bgDesktop}
-				alt='Planet Earth in Space'
-				placeholder='blur'
-				quality={100}
-				fill
-				sizes='100vw'
-				style={{
-					objectFit: 'cover',
-				}}
-				className='hidden lg:block'
-			/>
-			<Image
-				src={bgTablet}
-				alt='Planet Earth in Space'
-				placeholder='blur'
-				quality={100}
-				fill
-				sizes='100vw'
-				style={{
-					objectFit: 'cover',
-				}}
-				className='hidden md:block lg:hidden'
-			/>
-			<Image
-				src={bgMobile}
-				alt='Planet Earth in Space'
-				placeholder='blur'
-				quality={100}
-				fill
-				sizes='100vw'
-				style={{
-					objectFit: 'cover',
-				}}
-				className='md:hidden'
-			/>
-			<div className='pt-12 px-12 lg:pt-24 lg:px-24 max-w-screen-lg mx-auto flex flex-col h-full'>
-				<div className='mt-[75px] flex justify-start items-center relative z-20'>
-					<h5 className='text-white'>
-						<span className='opacity-25 font-bold'>02</span> Meet Your Crew
-					</h5>
-				</div>
-				<div className='flex-1 flex flex-col lg:grid lg:grid-cols-2 justify-stretch relative z-20 lg:-mt-8'>
-					<div className='max-w-2/3 flex flex-col justify-center items-center lg:gap-32 lg:items-start'>
-						{content.map(
-							(c, i) =>
-								tab === i && (
-									<motion.div
-										key={c.name}
-										initial={{ x: 10, opacity: 0 }}
-										animate={{ x: 0, opacity: 1 }}
-										exit={{ x: -10, opacity: 0 }}
-										transition={{ ease: 'easeOut', duration: 0.6 }}
-									>
-										<h4>{c.role}</h4>
-										<h3>{c.name}</h3>
-										<p className='text-lavender'>{c.bio}</p>
-									</motion.div>
-								)
-						)}
-						<div className='flex gap-6'>
-							{content.map((c, i) => (
-								<button
-									key={i}
-									onClick={() => setTab(i)}
-									className={`h-4 w-4 bg-slate-500 rounded-full transition hover:bg-white ${
-										tab === i ? 'selected' : ''
-									}`}
-								></button>
-							))}
-						</div>
-					</div>
-					<div className='flex justify-end items-end relative overflow-hidden'>
-						{content.map(
-							(c, i) =>
-								tab === i && (
-									<motion.div
-										key={c.name}
-										initial={{ y: 10, opacity: 0 }}
-										animate={{ y: 0, opacity: 1 }}
-										exit={{ y: -10, opacity: 0 }}
-										transition={{ ease: 'easeOut', duration: 0.6 }}
-										className='z-30 relative w-full h-full'
-									>
-										<Image
-											key={c.name}
-											src={c.images.png}
-											fill
-											alt={`Headshot of ${c.name}`}
-											style={{
-												objectFit: 'contain',
-											}}
-										/>
-									</motion.div>
-								)
-						)}
-					</div>
-				</div>
-			</div>
-		</main>
-	);
+  const content = data.crew;
+  const [tab, setTab] = useState(0);
+  return (
+    <main className="absolute left-0 top-0 h-screen w-full">
+      <Image
+        src={bgDesktop}
+        alt="Planet Earth in Space"
+        placeholder="blur"
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover",
+        }}
+        className="hidden lg:block"
+      />
+      <Image
+        src={bgTablet}
+        alt="Planet Earth in Space"
+        placeholder="blur"
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover",
+        }}
+        className="hidden md:block lg:hidden"
+      />
+      <Image
+        src={bgMobile}
+        alt="Planet Earth in Space"
+        placeholder="blur"
+        quality={100}
+        fill
+        sizes="100vw"
+        style={{
+          objectFit: "cover",
+        }}
+        className="md:hidden"
+      />
+      <div className="mx-auto flex h-full max-w-screen-lg flex-col px-12 pt-12 lg:px-24 lg:pt-24">
+        <div className="relative z-20 mt-[75px] flex items-center justify-start">
+          <h5 className="text-white">
+            <span className="font-bold opacity-25">02</span> Meet Your Crew
+          </h5>
+        </div>
+        <div className="relative z-20 flex flex-1 flex-col items-center justify-center lg:-mt-8 lg:grid lg:grid-cols-2">
+          <div className="max-w-2/3 flex max-w-[595px] flex-col items-center justify-center gap-10 text-center lg:max-w-full lg:items-start lg:gap-32 lg:text-left">
+            {content.map(
+              (c, i) =>
+                tab === i && (
+                  <motion.div
+                    key={c.name}
+                    initial={{ x: 10, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    exit={{ x: -10, opacity: 0 }}
+                    transition={{ ease: "easeOut", duration: 0.6 }}
+                    className="mt-[60px] lg:m-0"
+                  >
+                    <h4 className="text-[rgba(255,255,255,0.5)] md:text-2xl lg:text-white">
+                      {c.role}
+                    </h4>
+                    <h3 className="md:text-[40px] lg:text-[56px] lg:text-white">
+                      {c.name}
+                    </h3>
+                    <p className="text-balance text-[16px] leading-8 text-lavender lg:text-left lg:text-[18px] lg:leading-9">
+                      {c.bio}
+                    </p>
+                  </motion.div>
+                ),
+            )}
+            <div className="flex gap-6">
+              {content.map((c, i) => (
+                <button
+                  key={i}
+                  onClick={() => setTab(i)}
+                  className={`h-[10px] w-[10px] rounded-full bg-slate-500 transition hover:bg-white lg:h-4 lg:w-4 ${
+                    tab === i ? "selected" : ""
+                  }`}
+                ></button>
+              ))}
+            </div>
+          </div>
+          <div className="relative flex h-full w-full items-end justify-end overflow-hidden">
+            {content.map(
+              (c, i) =>
+                tab === i && (
+                  <motion.div
+                    key={c.name}
+                    initial={{ y: 10, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: -10, opacity: 0 }}
+                    transition={{ ease: "easeOut", duration: 0.6 }}
+                    className="relative z-30 h-[90%] w-full lg:h-full"
+                  >
+                    <Image
+                      key={c.name}
+                      src={c.images.png}
+                      fill
+                      alt={`Headshot of ${c.name}`}
+                      style={{
+                        objectFit: "contain",
+                      }}
+                    />
+                  </motion.div>
+                ),
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
