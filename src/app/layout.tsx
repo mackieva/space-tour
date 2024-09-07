3a63a4a5777108c7fe9095044f16d4455e20e91f
Ryan McGuire <rmva@ryans-mbp-2.myfiosgateway.com>
2024-09-06 20:49:38 -0400

    page transition and prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,42 +4,42 @@ import './globals.css';
 import Header from '@/components/ui/Header';
 
 const bellefair = Bellefair({
-	subsets: ['latin'],
-	weight: '400',
-	variable: '--font-belle',
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-belle',
 });
 
 const barlow = Barlow({
-	subsets: ['latin'],
-	weight: ['400', '600'],
-	variable: '--font-barlow',
+    subsets: ['latin'],
+    weight: ['400', '600'],
+    variable: '--font-barlow',
 });
 
 const barlow_condensed = Barlow_Condensed({
-	subsets: ['latin'],
-	weight: '400',
-	variable: '--font-barlow-con',
+    subsets: ['latin'],
+    weight: '400',
+    variable: '--font-barlow-con',
 });
 
 export const metadata: Metadata = {
-	title: 'Space Tourism',
-	description:
-		'Space tourism site with Next.js, Tailwind and Framer Motion / GSAP',
+    title: 'Space Tourism',
+    description:
+        'Space tourism site with Next.js, Tailwind and Framer Motion / GSAP',
 };
 
 export default function RootLayout({
-	children,
+    children,
 }: Readonly<{
-	children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<body
-				className={`${bellefair.variable} ${barlow_condensed.variable} ${barlow.variable} antialiased bg-blue`}
-			>
-				<Header />
-				{children}
-			</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <body
+                className={`${bellefair.variable} ${barlow_condensed.variable} ${barlow.variable} bg-blue antialiased`}
+            >
+                <Header />
+                {children}
+            </body>
+        </html>
+    );
 }
