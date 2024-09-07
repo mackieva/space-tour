5c0fee0ed8c856e3729069020eb886b5e3cee788
Ryan McGuire <rmva@ryans-mbp-2.myfiosgateway.com>
2024-09-07 10:04:23 -0400

    full tab complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,22 +43,24 @@ export default function Home() {
                 }}
                 className="md:hidden"
             />
-            <div className="mx-w-screen-lg z-2 relative mx-auto flex flex-col px-[24px] pt-[112px] md:h-full md:px-[160px] md:pt-[200px] lg:grid lg:h-[85vh] lg:grid-cols-2 lg:px-[165px]">
-                <div className="flex flex-col items-center justify-start lg:items-start lg:justify-center">
+            <div className="mx-w-screen-lg z-2 relative mx-auto flex h-full flex-col items-center justify-center px-[24px] pt-[112px] md:px-[160px] md:pt-[200px] lg:grid lg:grid-cols-2 lg:px-[165px] lg:pb-[130px] lg:pt-0">
+                <div className="flex max-w-[450px] flex-col items-center justify-start md:justify-center lg:items-start lg:justify-end">
                     <h5 className="subheadingTwo md:text-xl lg:text-[28px]">
                         So, you want to travel to
                     </h5>
-                    <h1 className="text-[80px] md:text-[150px]">Space</h1>
-                    <p className="mb-[81px] text-center text-lavender md:mb-0 md:text-base md:leading-[28px] lg:text-left lg:text-[15px]">
+                    <h1 className="text-[80px] leading-[1.35] md:text-[150px]">
+                        Space
+                    </h1>
+                    <p className="text-balance text-center text-lavender md:mb-0 md:text-base md:leading-[28px] lg:text-left lg:text-[18px] lg:leading-[32px]">
                         Let’s face it; if you want to go to space, you might as
                         well genuinely go to outer space and not hover kind of
                         on the edge of it. Well sit back, and relax because
                         we’ll give you a truly out of this world experience!
                     </p>
                 </div>
-                <div className="flex flex-col items-center justify-center md:mb-[90px] md:mt-auto lg:items-end lg:justify-end">
+                <div className="flex flex-col items-center justify-center md:mb-[90px] md:mt-auto lg:mb-0 lg:items-end lg:justify-end">
                     <Link
-                        className="rounded-full bg-white px-[45px] py-[85px] font-serif text-[32px] uppercase text-black lg:px-[65px] lg:py-[110px]"
+                        className="flex items-center justify-center rounded-full bg-white py-[85px] font-serif text-[32px] uppercase tracking-[2px] text-black md:h-[242px] md:w-[242px] md:px-0 lg:px-[65px] lg:py-[110px]"
                         href="/destination"
                     >
                         Explore
